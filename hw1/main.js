1c151227d319a7325ae8a0d5e0291b98bca7c323
Zepp <Zigerlcute@gmail.com>
2024-02-10 12:31:59 +0200

    chegfo

---
+++ hw1/main.js
@@ -0,0 +1,11 @@
+let userNickname = "alexei shurshilov";
+let userAge = "988";
+let userinfo = userNickname + ", " + userAge;
+console.log ('infa =', userinfo)
+let userScore = 1271;
+console.log('Score=', userScore)
+let price = Number(prompt("цена"));
+let quantity = Number(prompt("количество"));
+let total = price * quantity;
+alert("общая цена  " + total);
+console.log(price*quantity)
---
+++ hw2/main.js
@@ -0,0 +1,54 @@
+// let num1 = Number(prompt("Введіть перше число"));
+// let num2 = Number(prompt("Введіть друге число"));
+// if (num1 > num2) {
+//   console.log("Перше число більше за друге");
+// } else if (num1 < num2) {
+//   console.log("Перше число менше за друге");
+// } else {
+//   console.log("Числа рівні");
+// }
+// let num = Number(prompt("Введіть число"));
+// if (num % 2 === 0) {
+//   console.log("Число парне");
+// } else {
+//   console.log("Число непарне");
+// }
+// let grade = Number(prompt("Яка ваша оцінка за 10-бальною шкалою?"));
+// if (grade >= 8 && grade <= 10) {
+//   console.log("Відмінно");
+// } else if (grade >= 5 && grade <= 7) {
+//   console.log("Добре");
+// } else if (grade >= 3 && grade <= 4) {
+//   console.log("Задовільно");
+// } else if (grade < 3) {
+//   console.log("Незадовільно");
+// } else {
+//   console.log("Некоректне значення");
+// }
+// let age = Number(prompt("Скільки вам років?"));
+// let currentYear = 2024;
+// let birthYear = currentYear - age;
+// console.log("Ваш рік народження - " + birthYear);
+// let num = Number(prompt("Введіть число"));
+// if (num >= 1 && num <= 100) {
+//   console.log("Число знаходиться в діапазоні");
+// } else {
+//   console.log("Число поза діапазоном");
+// }
+// let guess = Number(prompt("Вгадайте число від 1 до 20"));
+// let number = 10;
+// if (guess > number) {
+//   console.log("Занадто велике");
+// } else if (guess < number) {
+//   console.log("Занадто мале");
+// } else {
+//   console.log("Вітаємо, ви вгадали!");
+// }
+// let result = '';
+// let score = 0;
+// result = score > 50 ? "Пройшов" : "Не пройшов";
+// console.log(result); 
+// let tempResult = "";
+// let temperature = 30;
+// tempResult = temperature > 25 ? "Гаряче" : "Прохолодно";
+// console.log(tempResult);
